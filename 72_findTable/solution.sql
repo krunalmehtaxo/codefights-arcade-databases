21041e00ed15b81a3ffa01ee7f73f9dfac10dfcb
Krunal Mehta <40229815+krunalmehtaxo@users.noreply.github.com>
2020-10-18 14:50:36 +0530

    Update solution.sql
    Initial query returned output as DATA_TYPE instead of data_type. Added an alias for data_type.

--- 72_findTable/solution.sql
+++ 72_findTable/solution.sql
@@ -1,7 +1,7 @@
 /*Please add ; after each select statement*/
 CREATE PROCEDURE findTable()
 BEGIN
-	SELECT table_name AS tab_name, column_name AS col_name, data_type
+	SELECT table_name AS tab_name, column_name AS col_name, data_type AS data_type
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE table_schema='ri_db' AND table_name REGEXP '^e.*s$'
     ORDER BY  tab_name, col_name;
